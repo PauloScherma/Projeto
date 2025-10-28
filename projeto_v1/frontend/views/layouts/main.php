7ccc52e264164618bccbddc3df79ab81ad2c362d
PASTEL2240097 <2240097@my.ipleiria.pt>
2025-10-28 16:52:05 +0000

    footter fixed f wierd php function

--- projeto_v1/frontend/views/layouts/main.php
+++ projeto_v1/frontend/views/layouts/main.php
@@ -89,12 +89,8 @@ AppAsset::register($this);
     </div>
 </main>
 
-<!--<footer class="footer mt-auto py-3 text-muted">
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>-->
+<?php //$this->endBody() ?> <!-- Sera que vai ser utilizado futuramente -->
+</body>
 
 <!-- Footer Start -->
 <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
@@ -160,27 +156,23 @@ AppAsset::register($this);
 </div>
 <!-- Footer End -->
 
+<!-- Back to Top -->
+<a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+<!-- JavaScript Libraries --><!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/wow/wow.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/easing/easing.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/waypoints/waypoints.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/counterup/counterup.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/owlcarousel/owl.carousel.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/tempusdominus/js/moment.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/tempusdominus/js/moment-timezone.min.js"></script>
+<script src="<?= Yii::$app->request->baseUrl ?>/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="../lib/wow/wow.min.js"></script>
-    <script src="../lib/easing/easing.min.js"></script>
-    <script src="../lib/waypoints/waypoints.min.js"></script>
-    <script src="../lib/counterup/counterup.min.js"></script>
-    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="../lib/tempusdominus/js/moment.min.js"></script>
-    <script src="../lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="../lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-
-    <!-- Template Javascript -->
-    <script src="../js/main.js"></script>
-
-<?php $this->endBody() ?>
-</body>
+<!-- Template Javascript -->
+<script src="<?= Yii::$app->request->baseUrl ?>/js/main.js"></script>
 </html>
 <?php $this->endPage();
