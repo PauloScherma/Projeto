058fcbd41b1b2cd796ad900fc01a173b43ee2829
Paulo Scherma <pauloantonioschermapinto@gmail.com>
2025-10-31 15:03:22 +0000

    Mudanças no RBAC, adicionadas mais permissões!

--- projeto_v1/console/controllers/RbacController.php
+++ projeto_v1/console/controllers/RbacController.php
@@ -13,6 +13,8 @@ class RbacController extends Controller
         $auth->removeAll(); // limpa tudo
 
         //------Permissoes------
+
+        //tabela request
         $viewRequest = $auth->createPermission('viewRequest');
         $viewRequest->description = 'Ver os pedidos';
         $auth->add($viewRequest);
@@ -29,30 +31,45 @@ class RbacController extends Controller
         $deleteRequest->description = 'Remover o pedido';
         $auth->add($deleteRequest);
 
-        $viewRequestHistory = $auth->createPermission('viewRequestHistory');
-        $viewRequestHistory->description = 'Ver o histórico de request';
-        $auth->add($viewRequestHistory);
-
         $cancelRequest = $auth->createPermission('cancelRequest');
         $cancelRequest->description = 'Cancelar o pedido';
         $auth->add($cancelRequest);
 
-        $rateRequest = $auth->createPermission('rateRequest');
-        $rateRequest->description = 'Criar o pedido';
-        $auth->add($rateRequest);
+        $changePriority = $auth->createPermission('changePriority');
+        $changePriority->description = 'Atualizar a priporidade';
+        $auth->add($changePriority);
 
-        $talkToTechnician = $auth->createPermission('talkToTechnician');
-        $talkToTechnician->description = 'Permite falar com o tecnico especifico';
-        $auth->add($talkToTechnician);
+        $changeStatus = $auth->createPermission('changestatus');
+        $changeStatus -> description = 'Atualizar o status do request';
+        $auth->add($changeStatus);
 
+        //tavela request_rate
+        $viewRating = $auth->createPermission('viewrating');
+        $viewRating -> description = 'Ver os ratings';
+        $auth->add($viewRating);
+
+        $createRating = $auth->createPermission('createrating');
+        $createRating -> description = 'Criar um rating';
+        $auth->add($createRating);
+
+        $updateRating = $auth->createPermission('updaterating');
+        $updateRating -> description = 'Atualizar um rating';
+        $auth->add($updateRating);
+
+        $deleteRating = $auth->createPermission('deleterating');
+        $deleteRating -> description = 'Remover um rating';
+        $auth->add($deleteRating);
+
+        //tabela request_assigment
         $assignTechnician = $auth->createPermission('assignTechnician');
         $assignTechnician->description = 'Atribuir técnico';
         $auth->add($assignTechnician);
 
-        $viewDashboard = $auth->createPermission('viewDashboard');
-        $viewDashboard->description = 'Ver o painel geral';
-        $auth->add($viewDashboard);
+        $changeTechnician = $auth->createPermission('changeTechnician');
+        $changeTechnician->description = 'Muda o técnico';
+        $auth->add($changeTechnician);
 
+        //tabela user
         $viewUsers = $auth->createPermission('viewUsers');
         $viewUsers->description = 'Ver os utilizadores';
         $auth->add($viewUsers);
@@ -68,6 +85,53 @@ class RbacController extends Controller
         $deleteUser = $auth->createPermission('deleteUser');
         $deleteUser->description = 'Remover o utilizador';
         $auth->add($deleteUser);
+
+        $validateBudget = $auth->createPermission('validatebudget');
+        $validateBudget->description = 'Validar o budget do request';
+        $auth->add($validateBudget);
+
+        $changeAvailability = $auth->createPermission('changeavailability');
+        $changeAvailability -> description = 'Atualizar o disponibilidade do tecnico';
+        $auth->add($changeAvailability);
+
+        //tabela request_attachement
+        $viewReport = $auth->createPermission('viewreport');
+        $viewReport->description = 'Ver o report';
+        $auth->add($viewReport);
+
+        $createReport = $auth->createPermission('createreport');
+        $createReport -> description = 'Criar o report';
+        $auth->add($createReport);
+
+        $updateReport = $auth->createPermission('updatereport');
+        $updateReport -> description = 'Atualizar o report';
+        $auth->add($updateReport);
+
+        $deleteReport = $auth->createPermission('deletereport');
+        $deleteReport -> description = 'Remover o report';
+        $auth->add($deleteReport);
+
+        //tabela calendar_event
+        $viewAppointment = $auth->createPermission('viewappointment');
+        $viewAppointment -> description = 'Ver o marcação';
+        $auth->add($viewAppointment);
+
+        $createAppointment = $auth->createPermission('createappointment');
+        $createAppointment -> description = 'Criar o marcação';
+        $auth->add($createAppointment);
+
+        $updateAppointment = $auth->createPermission('updateappointment');
+        $updateAppointment -> description = 'Atualizar o marcação';
+        $auth->add($updateAppointment);
+
+        $deleteAppointment = $auth->createPermission('deleteappointment');
+        $deleteAppointment -> description = 'Remover o marcação';
+        $auth->add($deleteAppointment);
+
+        //outras
+        $viewDashboard = $auth->createPermission('viewDashboard');
+        $viewDashboard->description = 'Ver a dashboard';
+        $auth->add($viewDashboard);
 
         // ------ Roles ------
         $cliente = $auth->createRole('cliente');
